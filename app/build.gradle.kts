ef3189a8e2a8cb4afb9501ca5cfb88d9eb67956e
eks <eks@bam-boo.eu>
2023-03-07 16:09:47 +0200

    rename module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,5 +38,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(project(":VoiceAnimation"))
+    implementation(project(":speech-waves-animation"))
 }
